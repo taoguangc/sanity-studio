90378cdfd95a487d0237443411068796c7d52179
taoguangc <taoguangc@gmail.com>
2023-12-20 11:48:57 +0800

    upgrade and add locale

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,6 +3,7 @@ import {schemaTypes} from './schemas'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {media} from 'sanity-plugin-media'
+import { zhHansLocale } from '@sanity/locale-zh-hans'
 
 export default defineConfig({
   name: 'default',
@@ -11,7 +12,7 @@ export default defineConfig({
   projectId: 'k7xaxt1p',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool(), media()],
+  plugins: [deskTool(), visionTool(), media(), zhHansLocale()],
 
   schema: {
     types: schemaTypes,
